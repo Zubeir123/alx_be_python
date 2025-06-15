c225796a25f8b4b8f8ef47a4dffe2ee8c0a30059
zubeir abib <zubeirabib@gmail.com>
2025-06-15 10:05:13 +0300

    added test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,22 +1,22 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
-calc = SimpleCalculator()
 
 class TestSimpleCalculator(unittest.TestCase):
+    calc = SimpleCalculator()
     def test_addition(self):
-        self.assertEqual(calc.add(13, 7), 20)
-        self.assertEqual(calc.add(-1, 1), 0)
-        self.assertEqual(calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(13, 7), 20)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
     def test_subtract(self):
-        self.assertEqual(calc.subtract(13, 7), 6)
-        self.assertEqual(calc.subtract(-1, 1), -2)
-        self.assertEqual(calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(13, 7), 6)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
     def test_multiply(self):
-        self.assertEqual(calc.multiply(0, 1), 0)
-        self.assertEqual(calc.multiply(-1, 1), -1)
-        self.assertEqual(calc.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(0, 1), 0)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
     def test_divide(self):
-        self.assertEqual(calc.divide(21, 3), 7)
-        self.assertEqual(calc.divide(0, 1), 0)
-        self.assertEqual(calc.divide(8, 0), None)
+        self.assertEqual(self.calc.divide(21, 3), 7)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        self.assertEqual(self.calc.divide(8, 0), None)
